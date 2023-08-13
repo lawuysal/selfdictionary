c5e282e9dd997118984c3b2b4ba3b9e9b487600c
Oğuzhan Uysal <law-tekken3@hotmail.com>
2023-08-13 13:07:41 +0300

    Added Category Menu

---
+++ lib/testpage.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class TestPage extends ConsumerStatefulWidget {
+  const TestPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _TestPageState();
+}
+
+class _TestPageState extends ConsumerState<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Catogory Name'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Item $index'),
+                    leading: Text((index + 1).toString()),
+                    subtitle: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text('SubItem 1'),
+                            Text('SubItem 2'),
+                            Text('SubItem 3'),
+                            Text('SubItem 4'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
